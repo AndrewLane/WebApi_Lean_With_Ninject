eec5de8d8b50f0ae32e23dac4c7aa707ba3af5d2
Andrew Lane <andrew.w.lane@gmail.com>
2016-05-18 09:03:31 -0400

    * Add a bare-bones web api solution with a single hello world action in a dummy controller

---
+++ WebApi_Lean_With_Ninject/Controllers/DummyController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace WebApi_Lean_With_Ninject.Controllers
+{
+    public class DummyController : ApiController
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "World";
+        }
+    }
+}
